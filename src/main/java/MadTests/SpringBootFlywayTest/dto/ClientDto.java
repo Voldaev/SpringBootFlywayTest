77f07c37b5483a5a74cf7eb6c00c14fbc658e03f
aleksey.frolov <aleksey2093@outlook.com>
2022-02-12 20:30:16 +0300

    re

--- src/main/java/MadTests/SpringBootFlywayTest/dto/ClientDto.java
+++ src/main/java/MadTests/SpringBootFlywayTest/dto/ClientDto.java
@@ -1,6 +1,10 @@
 package MadTests.SpringBootFlywayTest.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,10 +13,25 @@ import lombok.*;
 @ToString
 @EqualsAndHashCode
 @Builder
+@JsonIgnoreProperties({"surname"})
 public class ClientDto {
 
     private Integer id;
     private String name;
     private String phone;
+
+    private String surname;
+    @JsonIgnore
+    private String firstname;
+    @JsonIgnore
+    private String middlename;
+
+    public String getFullName() {
+        return Arrays.asList(surname, firstname, middlename)
+                .stream()
+                .filter(x -> x != null)
+                .reduce((x1, x2) -> x1 + ", " + x2)
+                .orElse("");
+    }
 
 }
