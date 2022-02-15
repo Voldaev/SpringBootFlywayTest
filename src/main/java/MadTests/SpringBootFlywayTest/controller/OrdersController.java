ab0739ed5ee77f4138a23e0ea74f5377a5026197
Voldaev <lking13017@gmail.com>
2022-02-15 16:43:24 +0300

    api explanation

--- src/main/java/MadTests/SpringBootFlywayTest/controller/OrdersController.java
+++ src/main/java/MadTests/SpringBootFlywayTest/controller/OrdersController.java
@@ -4,6 +4,7 @@ import MadTests.SpringBootFlywayTest.dto.OrderDTO;
 import MadTests.SpringBootFlywayTest.dto.TimeWindowDTO;
 import MadTests.SpringBootFlywayTest.dto.WantedInputDTO;
 import MadTests.SpringBootFlywayTest.service.OrdersDBService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +15,13 @@ public class OrdersController {
     @Autowired
     OrdersDBService ordersDBService;
 
+    @Operation(summary = "Get all free time windows available atm")
     @GetMapping(value = "/orders/windows")
     public List<TimeWindowDTO> getAllFreeTime() {
         return ordersDBService.readAll();
     }
 
+    @Operation(summary = "Get all free time windows available for the offer. preferred start time optional")
     @GetMapping(value = "/orders/wanted")
     public List<TimeWindowDTO> getFreeTimeInWindow(@RequestParam(name = "offer") String offer,
                                                    @RequestParam(name = "hour", required = false, defaultValue = "-1") String hour,
@@ -27,6 +30,7 @@ public class OrdersController {
         return ordersDBService.read(wanted);
     }
 
+    @Operation(summary = "Add new order")
     @PostMapping(value = "/orders")
     public void createOrder(@RequestBody OrderDTO order) {
         ordersDBService.create(order);
